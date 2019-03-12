361216ccd261ec4b0926e3e6779c0ec395f72927
Kajal Mallick <kajal.mallick@cognizant.com>
2019-03-12 09:31:06 +0530

    Added new codes for router

--- 06_Angular_Assignment1/angular-assignment1/src/app/app-routing.module.ts
+++ 06_Angular_Assignment1/angular-assignment1/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo: "home", pathMatch: "full"},
+  {path:"home", component: AboutMeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
